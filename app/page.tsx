7850099139faf6935a46f652280acb955ae0dec3
natsu0001 <talukdarabhishek687@gmail.com>
2026-05-19 10:53:01 +0530

    test8

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,35 @@
 "use client";
 
+import { useState } from "react";
+
 import Sidebar from "./components/layout/Sidebar";
 import Navbar from "./components/layout/Navbar";
-import { useState } from "react";
+import PriceTicker from "./components/PriceTicker";
 
 export default function Home() {
   const [sidebarOpen, setSidebarOpen] =
     useState(false);
 
   return (
-    <div className="bg-black text-white min-h-screen">
+    <div className="flex bg-black min-h-screen text-white">
+
       <Sidebar
         open={sidebarOpen}
         setOpen={setSidebarOpen}
       />
 
-      <Navbar
-        sidebarOpen={sidebarOpen}
-        setSidebarOpen={setSidebarOpen}
-      />
+      <main className="flex-1 lg:ml-64">
+
+        <Navbar
+          sidebarOpen={sidebarOpen}
+          setSidebarOpen={setSidebarOpen}
+        />
+
+        <div className="p-6">
+          <PriceTicker />
+        </div>
+
+      </main>
     </div>
   );
 }
