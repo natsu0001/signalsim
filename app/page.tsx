c5949db44726231fb63584db773e07c4af6644ed
natsu0001 <talukdarabhishek687@gmail.com>
2026-05-19 10:49:38 +0530

    test7

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
 "use client";
 
 import Sidebar from "./components/layout/Sidebar";
+import Navbar from "./components/layout/Navbar";
+import { useState } from "react";
 
 export default function Home() {
+  const [sidebarOpen, setSidebarOpen] =
+    useState(false);
+
   return (
-    <div className="bg-black text-white">
+    <div className="bg-black text-white min-h-screen">
       <Sidebar
-        open={false}
-        setOpen={() => {}}
+        open={sidebarOpen}
+        setOpen={setSidebarOpen}
+      />
+
+      <Navbar
+        sidebarOpen={sidebarOpen}
+        setSidebarOpen={setSidebarOpen}
       />
     </div>
   );
